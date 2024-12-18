fcddd76f66d5509a7db6238fab1fbf9b913d4727
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-12-17 19:06:35 -0500

    PT1: Basic Format

---
+++ app/controllers/api/v1/tins_controller.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+module Api::V1
+	class TinsController < ApplicationController
+		def validationTin 
+			binding.pry
+
+
+		end
+	end
+end
+
---
+++ app/services/api/v1/tin_validation_service.rb
@@ -0,0 +1,12 @@
+class TinValidationService
+end
+
+
+
+
+
+
+
+
+
+
